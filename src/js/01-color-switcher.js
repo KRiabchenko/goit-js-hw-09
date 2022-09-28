ad185909c0b14ca2dae756f625ee8f238f5c2b0b
KRiabchenko <katerina.riabchenko90@gmail.com>
2022-09-28 13:03:43 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyRef: document.querySelector('body'),
+}
 
+let timerId = null;
+
+const backgroundColorSwitcher = function () {
+  refs.bodyRef.style.backgroundColor = getRandomHexColor();
+};
+
+refs.startBtn.addEventListener("click", () => {
+    timerId = setInterval(backgroundColorSwitcher, 1000);
+    refs.startBtn.disabled = true;
+});
+
+
+refs.stopBtn.addEventListener("click", () => {
+   clearInterval(timerId);
+  refs.startBtn.disabled = false;
+});
